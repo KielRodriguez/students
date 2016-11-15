fabffc904767feea5aad107d9b8132abc2f052b2
KielRodriguez <kr.rdz.20@gmail.com>
2016-11-14 18:00:15 -0600

    feat: add action methods index, new, show and create

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,33 @@
 # Controller Students
 class StudentsController < ApplicationController
   def index
-    # @students = Student.all
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
   end
 
   def new
+    @student = Student.new
+  end
+
+  def edit
+
   end
 
   def create
-    @student = Student.new(params[:student])
-    @student.save
-    redirect_to @student
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to @student
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    def student_params
+      params.require(:student).permit(:name, :last_name, :birth_date)
+    end
 end
